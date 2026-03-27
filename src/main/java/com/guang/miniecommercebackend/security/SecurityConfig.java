58d0d1fb5abcad49d1f84a3092b4002d13744c0e
Xiaoguang Wang <guangxiaozhi@hotmail.com>
2026-03-27 12:40:14 -0700

    change home page from '/' to '/produts' (#2)

--- src/main/java/com/guang/miniecommercebackend/security/SecurityConfig.java
+++ src/main/java/com/guang/miniecommercebackend/security/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(sm ->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/api/auth/**", "/error").permitAll()
+                        .requestMatchers("/products", "/api/auth/**", "/error").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(form -> form.disable())
                 .httpBasic(basic -> basic.disable())
