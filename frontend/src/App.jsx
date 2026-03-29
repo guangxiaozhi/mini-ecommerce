81fd7c6f4b75e0e0c603162457013f3c1f735b3a
xiaoguang <guangxiaozhi@hotmail.com>
2026-03-28 23:19:30 -0700

    add a header like amazon website's

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import RegisterForm from "./components/RegisterForm/RegisterForm.jsx";
 import LoginForm from "./components/LoginForm/LoginForm.jsx";
 import UserProfile from "./components/UserProfile/UserProfile.jsx";
+import Header from "./components/Header/Header.jsx";
 
 function App() {
   const [msg, setMsg] = useState('')
@@ -10,16 +11,15 @@ function App() {
   return (
     <>
 
-      <RegisterForm onMessage={setMsg} />
+        <Header />
+        <RegisterForm onMessage={setMsg} />
 
-      <LoginForm onMessage={setMsg} />
+        <LoginForm onMessage={setMsg} />
 
-      <UserProfile onMessage={setMsg} />
-      <pre
-        style={{ whiteSpace: 'pre-wrap', wordBreak: 'break-all' }}
-      >
-        {msg}
-      </pre>
+        <UserProfile onMessage={setMsg} />
+        <pre style={{ whiteSpace: 'pre-wrap', wordBreak: 'break-all' }} >
+          {msg}
+        </pre>
     </>
   )
 }
