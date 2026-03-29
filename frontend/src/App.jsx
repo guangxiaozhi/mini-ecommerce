bee3a87b827a50d68e66015e847ca9e67b25acf9
Xiaoguang Wang <guangxiaozhi@hotmail.com>
2026-03-29 15:38:52 -0700

    resove login/register button keep localstorage data issue (#6)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
+import { getMe} from "./api/auth.js";
 import './App.css'
 import UserProfile from './components/UserProfile/UserProfile.jsx'
 import Header from './components/Header/Header.jsx'
@@ -8,13 +9,10 @@ import AuthModal from './components/AuthModal/AuthModal.jsx'
 function App() {
   const [msg, setMsg] = useState('')
   const [authOpen, setAuthOpen] = useState(false)
-  const [userName, setUserName] = useState(
-    () => localStorage.getItem('username') || null
-  )
+  const [userName, setUserName] = useState(null)
 
   function handleLoggedIn(name) {
     if (name) {
-      localStorage.setItem('username', name)
       setUserName(name)
     }
     setAuthOpen(false)
@@ -26,6 +24,27 @@ function App() {
       setUserName(null)
   }
 
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+        if (!token) {
+            setUserName(null)
+            return
+        }
+
+        getMe(token)
+            .then((data) => {
+                if (data && data.username != null) {
+                    setUserName(data.username)
+                } else {
+                    setUserName(null)
+                }
+            })
+            .catch(() => {
+                localStorage.removeItem('token')
+                localStorage.removeItem('username')
+                setUserName(null)
+            })
+    }, [])
   return (
     <>
       <Header
@@ -45,9 +64,6 @@ function App() {
                 element={
                     <main>
                         <p>Products Coming Soon...</p>
-                        <pre style={{ whiteSpace: 'pre-wrap', wordBreak: 'break-all' }}>
-              {msg}
-            </pre>
                     </main>
                 }
             />
